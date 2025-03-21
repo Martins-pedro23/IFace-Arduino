bd4a5f0d3c9918c4ddae80fa1c8cf66a6228b6b9
João Pedro <107441401+Martins-pedro23@users.noreply.github.com>
2025-03-21 13:53:51 -0300

    Update arduinoCode.ino

--- arduinoCode/arduinoCode.ino
+++ arduinoCode/arduinoCode.ino
@@ -3,11 +3,6 @@ int LPWM_1 = 5;
 int RPWM_2 = 10;
 int LPWM_2 = 9;
 
-int state_RPWM_1 = 0;
-int state_LPWM_1 = 0;
-int state_RPWM_2 = 0;
-int state_LPWM_2 = 0;
-int lastMovment = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -16,7 +11,6 @@ void setup() {
   
   pinMode(RPWM_1, OUTPUT);
   pinMode(LPWM_1, OUTPUT);
-  
   pinMode(RPWM_2, OUTPUT);
   pinMode(LPWM_2, OUTPUT);
 
@@ -31,63 +25,19 @@ void motorStop() {
 
 void moveMotors(int direction) {
   // Para frente (F)
-  if (direction == 1 && state_RPWM_1 != 1 && state_RPWM_2 != 1) {
+  if (direction == 1) {
     analogWrite(RPWM_1, 255);
     analogWrite(LPWM_1, 0);
     analogWrite(RPWM_2, 255);
     analogWrite(LPWM_2, 0);
-    state_RPWM_1 = 1;
-    state_RPWM_2 = 1;
-    state_LPWM_1 = 0;
-    state_LPWM_2 = 0;
-    Serial.println('F');
-  } else if (direction == 1 && state_RPWM_1 != 1 && state_RPWM_2 == 1){
-    analogWrite(RPWM_1, 255);
-    analogWrite(LPWM_1, 0);
-    analogWrite(LPWM_2, 0);
-    state_RPWM_1 = 1;
-    state_RPWM_2 = 1;
-    state_LPWM_1 = 0;
-    state_LPWM_2 = 0;
-    Serial.println('F');
-  } else if (direction == 1 && state_RPWM_1 == 1 && state_RPWM_2 != 1){
-    analogWrite(RPWM_2, 255);
-    analogWrite(LPWM_1, 0);
-    analogWrite(LPWM_2, 0);
-    state_RPWM_1 = 1;
-    state_RPWM_2 = 1;
-    state_LPWM_1 = 0;
-    state_LPWM_2 = 0;
     Serial.println('F');
   }
   // Para trás (B)
-  else if (direction == 2 && state_LPWM_1 != 1 && state_LPWM_2 != 1){
+  else if (direction == 2){
     analogWrite(LPWM_2, 255);
     analogWrite(LPWM_1, 255);
     analogWrite(RPWM_1, 0);
     analogWrite(RPWM_2, 0);
-    state_RPWM_1 = 0;
-    state_RPWM_2 = 0;
-    state_LPWM_1 = 1;
-    state_LPWM_2 = 1;
-    Serial.println('B');
-  } else if (direction == 2 && state_LPWM_1 != 1 && state_LPWM_2 == 1){
-    analogWrite(LPWM_1, 255);
-    analogWrite(RPWM_1, 0);
-    analogWrite(RPWM_2, 0);
-    state_RPWM_1 = 0;
-    state_RPWM_2 = 0;
-    state_LPWM_1 = 1;
-    state_LPWM_2 = 1;
-    Serial.println('B');
-  } else if (direction == 2 && state_LPWM_1 == 1 && state_LPWM_2 != 1){
-    analogWrite(LPWM_2, 255);
-    analogWrite(RPWM_1, 0);
-    analogWrite(RPWM_2, 0);
-    state_RPWM_1 = 0;
-    state_RPWM_2 = 0;
-    state_LPWM_1 = 1;
-    state_LPWM_2 = 1;
     Serial.println('B');
   }
 }
@@ -101,32 +51,19 @@ void loop() {
   switch (state.charAt(0)){
     case 'F':  // Movimento para frente
       moveMotors(1);
-      delay(1000);  // Move para frente
+      delay(700);  
       motorStop();
-      lastMovment = 1;
       break;
     case 'B':  // Movimento para trás
       moveMotors(2);
-      delay(1000);  // Move para frente
+      delay(700);  
       motorStop();
-      lastMovment = 2;
       break;
 
-    default:
-      if(lastMovment == 1){
-        moveMotors(2);
-      } else if(lastMovment == 2) {
-        moveMotors(1);
-      }
-      delay(1000);  // Move para frente
+    case 'N':
       motorStop();
-      lastMovment = 0;
-      state_RPWM_1 = 0;
-      state_RPWM_2 = 0;
-      state_LPWM_1 = 0;
-      state_LPWM_2 = 0;
       break;
   }
-  delay(1000);
+  delay(100);
   Serial.flush();
 }
